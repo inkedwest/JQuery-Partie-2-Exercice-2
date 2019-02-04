808c5c640352d4e0f0907fcb57134c2e47ccd0af
Glatigny <f.west@live.fr>
2019-02-04 12:02:38 +0100

    exo 2

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+$('#image').dblclick(function(){ // On appelle la fonction au double clic
+    $('#image').css('width', '500px'); // Au double clic, modif du CSS et de la largeur de l'image à 500px
+});
